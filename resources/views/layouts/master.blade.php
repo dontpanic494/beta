a726cf85f425cad71899306a2cdf5339d29ceddf
John Lincoln <jlincoln88@gmail.com>
2016-04-19 00:29:07 -0400

    Created layout folder and master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		
+	@yied('title', 'beta.johnlincoln.me')
+
+	</title>
+
+	<meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/bootstrapDarkly.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+</html>
